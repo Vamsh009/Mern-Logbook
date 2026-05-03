ae15af518ce57355aa851a8b2c8ccf5df4873665
Vamsh Amin <vamshamin333@gmail.com>
2026-05-03 13:50:28 +0530

    pepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from "dotenv";
+import path from 'path'
 
 
 import notesRoute from './routes/notesRoute.js';
@@ -16,11 +17,16 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 app.use(express.json());//middleware allows to parse JSON bodies
-app.use(cors({
-  origin: 'http://localhost:5173', // Allow requests from this origin
-})); // Enable CORS for all routes
+
+if(process.env.NODE_ENV !== "production"){
+
+  app.use(cors({
+    origin: 'http://localhost:5173', // Allow requests from this origin
+  })); // Enable CORS for all routes
+}
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);   // simple custom middleware
@@ -31,6 +37,14 @@ app.use((req, res, next) => {
 
 
 app.use('/api/notes', notesRoute);
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+  app.get("*" , (req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+});
+}
 
 
 
